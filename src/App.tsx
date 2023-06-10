659630b68d57e1384f7a4378a9ab2e9166b82688
Olha Pavlenko <olgaigpavlenko@gmail.com>
2023-06-10 18:37:05 +0100

    corrected routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, ReactNode } from 'react';
 import { BrowserRouter, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Login } from './Login/Login';
 import { Layout } from './pages/Layout/Layout';
-import { Main } from './pages/Main/Main';
-import { NoPage } from './pages/NoPage/NoPage';
-import { SingUp } from './SingUp/SingUp';
 import './App.css';
-import { ROUTE_PATHS } from './constants/constants';
+import { ROUTE_LAYOUT, ROUTE_PATHS } from './constants/constants';
 
 const App: FunctionComponent = () => {
+  let render: ReactNode;
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path={ROUTE_PATHS.layout} element={<Layout />}>
-            <Route index element={<Main />} />
-            <Route path={ROUTE_PATHS.login} element={<Login />} />
-            <Route path={ROUTE_PATHS.singUp} element={<SingUp />} />
-            <Route path={ROUTE_PATHS.noPage} element={<NoPage />} />
+          <Route path={ROUTE_LAYOUT.path} element={<Layout />}>
+            {ROUTE_PATHS.map((item, index) => {
+              item.path === 'main'
+                ? (render = <Route key={index} index path={item.path} element={item.component} />)
+                : (render = <Route key={index} path={item.path} element={item.component} />);
+              return render;
+            })}
           </Route>
         </Routes>
       </BrowserRouter>
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,24 @@
-export const ROUTE_PATHS = {
-  layout: '/',
-  login: 'login',
-  singUp: 'createAcc',
-  noPage: '*',
+export const ROUTE_PATHS = [
+  {
+    path: 'main',
+    component: '<Main />',
+  },
+  {
+    path: 'login',
+    component: '<Login />',
+  },
+  {
+    path: 'createAcc',
+    component: '<SingUp />',
+  },
+  {
+    path: '*',
+    component: '<NoPage />',
+  },
+];
+
+export const ROUTE_LAYOUT = {
+  path: '/',
+  component: '<Layout />',
 };
 
